76e4ffaf6d124683e8f6f9ba0dc4921e9bb18596
Jim Edlin <jim@kiva.org>
2017-08-07 11:48:24 -0700

    turn page heading into component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import React, { Component } from 'react';
+
 import TeamList from './components/TeamList';
+import PageHeading from './components/PageHeading';
 import './css/extracted.css';
 import './css/imported.css';
 
-
-
 class App extends Component {
-  render() {
+  render(props) {
     return (
       <div className="App">
         <div className="container">
           <div className="row">
             <div className="columns small-12 large-8">
-              <div className="teams-overview">
-                <h1 className="page-title">Lending Teams</h1>
-                <h2>Connect with Kiva's lender community</h2>
-                <p>Lending teams are self-organized groups built around common interests, school affiliation or location.</p>
-                <p>Join as many teams as you'd like, and rally around shared lending goals by counting each of your loans at checkout toward the team's impact.</p>
-                <p>To get started, simply join an existing team or create a new one and invite your friends!</p>
-              </div>
+
+                <PageHeading
+                  class = {"teams-overview"}
+                  headline = {"Lending Teams"}
+                  subhead = {"Connect with Kiva's lender community"}
+                  text = {
+                    <div>
+                      <p>Lending teams are self-organized groups built around common interests, school affiliation or location.</p>
+                      <p>Join as many teams as you'd like, and rally around shared lending goals by counting each of your loans at checkout toward the team's impact.</p>
+                      <p>To get started, simply join an existing team or create a new one and invite your friends!</p>
+                    </div>
+                  }/>
 
               <TeamList />
 
